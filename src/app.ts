d1becbc8f0218fc3324615eba71328e8c6f75736
Abul Ala Jobayar <jobayar59@gmail.com>
2023-11-19 07:21:21 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
